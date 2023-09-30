a0d1e9964b57aaed62e2704f08cce1164e0ee765
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2023-09-30 15:48:16 -0600

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/logo1.png'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
@@ -20,18 +23,20 @@ const Navbar = () => {
         </div> 
 
         {/* Hamburger */}
-        <div className='md:hidden'>
-            <FaBars />
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
         </div>
 
         {/* Mobile menu */}
-        <ul className='hidden'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Contact</li>
         </ul>
+        
+        {/* finished at 23:00 */}
 
         {/* Social icons */}
         <div className='hidden'>

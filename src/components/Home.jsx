ee7780d95862b9a75d05a7a12238ad8020f60208
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2023-09-30 22:49:56 -0600

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 const Home = () => {
   return (
     <div className='w-full h-screen bg-[#0a192f]'>
-        {/* finished at 26:18 */}
+        {/* finished at 32:58 */}
     </div>
   )  
 }

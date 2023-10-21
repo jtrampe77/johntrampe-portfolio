7b6597e7679139ff9a9c1f497e6a6389e93fdabd
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2023-10-21 16:52:11 -0600

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            {/* <img src={Logo} alt="JT Logo" style={{width:'70px'}} /> */}
-            <p>John Trampe</p>
+            <img src={Logo} alt="JT Logo" style={{width:'150px'}} />
         </div>
 
         {/* Menu */}
